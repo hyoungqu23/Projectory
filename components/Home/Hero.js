c24ac28ac117b6e6c2bbd9e9264169a041893fae
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-07-03 01:54:54 +0900

    [feat] 새로고침 방지를 위해 Link 태그 추가

--- components/Home/Hero.js
+++ components/Home/Hero.js
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 const Hero = () => {
   return (
     <div className="xl:w-5/12 flex flex-col justify-center items-center lg:items-start sm:text-center lg:text-left lg:py-12 xl:py-24">
@@ -12,10 +14,11 @@ const Hero = () => {
         <br />
         그것만으로도 당신의 프로젝트가 빛나게 될 거에요!
       </p>
-
-      <button className="inline-block bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded outline-none transition duration-100 px-8 py-2">
-        지금까지의 프로젝트 확인하러 가기
-      </button>
+      <Link href="/projects">
+        <button className="inline-block bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded outline-none transition duration-100 px-8 py-2">
+          지금까지의 프로젝트 확인하러 가기
+        </button>
+      </Link>
     </div>
   );
 };
