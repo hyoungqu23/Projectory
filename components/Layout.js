26cd0df295a6bb94a6d211e35bc4b7ae75cdca91
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-07-03 09:53:43 +0900

    [feat] 다크모드 구현

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import Footer from './Footer';
 
 const Layout = ({ children }) => {
   return (
-    <div>
+    <div className="bg-primary">
       <Header />
       <div>{children}</div>
       <Footer />
--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="bg-white pb-6 sm:pb-8 lg:pb-12">
+      <div className="bg-white pb-6 sm:pb-8 lg:pb-12 bg-primary">
         <div className="max-w-screen-2xl px-4 md:px-8 mx-auto">
           <section className="flex flex-col lg:flex-row justify-between gap-2 sm:gap-5 md:gap-8">
             <Hero />
